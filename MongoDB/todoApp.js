aeaf0aa326ce65c78ce407372e94d1c46190468f
toyzaaa5 <toy_2539@hotmail.com>
2017-06-16 17:22:29 +0700

    MongoDB

--- MongoDB/todoApp.js
+++ MongoDB/todoApp.js
@@ -19,7 +19,7 @@ MongoClient.connect(url, function(err, database){
 });
 
 //insert
-app.post('/task', function(req, res){
+var insertDocuments = function(req, res, urlx) {
   var json = req.body;
   var doc = {'ID':parseInt(json.id),
               'Description':json.Description,
@@ -29,113 +29,57 @@ app.post('/task', function(req, res){
     res.send('Something wrong???', 400);
   }
   else {
-  db.collection('task').insert(doc, {w:1}, function(err, result){
-    db.collection('task').findOne({ ID: parseInt(json.id)},function(err, docs){
+  db.collection(urlx).insert(doc, {w:1}, function(err, result){
+    db.collection(urlx).findOne({ ID: parseInt(json.id)},function(err, docs){
         res.send(docs,201);
     });
   });
   }
-});
-
-app.post('/work', function(req, res){
-  var json = req.body;
-  var doc = {'ID':parseInt(json.id),
-              'Description':json.Description,
-              'Completed':json.Completed,
-              'Duration':json.Duration};
-  if(json.id == null||typeof json.Description !== 'string'||typeof json.Completed !== 'boolean'||typeof json.Duration !== 'string'){
-    res.send('Something wrong???', 400);
-  }
-  else {
-  db.collection('work').insert(doc, {w:1}, function(err, result){
-    db.collection('work').findOne({ ID: parseInt(json.id)},function(err, docs){
-        res.send(docs,201);
-    });
-  });
-  }
-});
+}
 
 //list
-app.get('/task', function(req, res){
-    db.collection('task').find().toArray(function(err, docs){
-      if(docs == ''){
-        res.send('Not found :-(', 404);
-      } else {
-        res.send(docs);}
-    });
-});
-
-app.get('/work', function(req, res){
-    db.collection('work').find().toArray(function(err, docs){
-      if(docs == ''){
-        res.send('Not found :-(', 404);
-      } else {
-        res.send(docs);}
-    });
-});
+var listDocuments = function(req, res, urlx) {
+  db.collection(urlx).find().toArray(function(err, docs){
+    if(docs == ''){
+      res.send('Not found :-(', 404);
+    } else {
+      res.send(docs);}
+  });
+}
 
 //get
-app.get('/task/:id', function(req, res){
-    if(isNaN(parseInt(req.params.id)) == true){
-      res.send('Wrong Input!!! (Maybe it is not number)', 400);
-    }
-    else{
-    var myquery = { ID: parseInt(req.params.id)};
-    db.collection('task').findOne(myquery,function(err, docs){
-      if(docs == null){
-        res.send('Not found :-(', 404);
-      } else {
-        res.send(docs);}
-    });
-    }
-});
-
-app.get('/work/:id', function(req, res){
-    if(isNaN(parseInt(req.params.id)) == true){
-      res.send('Wrong Input!!! (Maybe it is not number)', 400);
-    }
-    else{
-    var myquery = { ID: parseInt(req.params.id)};
-    db.collection('work').findOne(myquery,function(err, docs){
-      if(docs == null){
-        res.send('Not found :-(', 404);
-      } else {
-        res.send(docs);}
-    });
-    }
-});
+var getDocuments = function(req, res, urlx) {
+  if(isNaN(parseInt(req.params.id)) == true){
+    res.send('Wrong Input!!! (Maybe it is not number)', 400);
+  }
+  else{
+  var myquery = { ID: parseInt(req.params.id)};
+  db.collection(urlx).findOne(myquery,function(err, docs){
+    if(docs == null){
+      res.send('Not found :-(', 404);
+    } else {
+      res.send(docs);}
+  });
+  }
+}
 
 //delete
-app.delete('/task/:id', function(req, res){
+var deleteDocuments = function(req, res, urlx) {
   if(isNaN(parseInt(req.params.id)) == true){
     res.send('Wrong Input!!! (Maybe it is not number)', 400);
   }
   else{
   var myquery = { ID: parseInt(req.params.id)};
-  db.collection('task').remove(myquery, function(err, docs){
-    db.collection('task').findOne(myquery,function(err, docs){
+  db.collection(urlx).remove(myquery, function(err, docs){
+    db.collection(urlx).findOne(myquery,function(err, docs){
         res.send(docs,204);
     });
   });
   }
-});
-
-app.delete('/work/:id', function(req, res){
-  if(isNaN(parseInt(req.params.id)) == true){
-    res.send('Wrong Input!!! (Maybe it is not number)', 400);
-  }
-  else{
-  var myquery = { ID: parseInt(req.params.id)};
-  db.collection('work').remove(myquery, function(err, docs){
-    db.collection('work').findOne(myquery,function(err, docs){
-        res.send(docs,204);
-    });
-  });
-  }
-});
+}
 
 //update
-app.patch('/task/:id', function(req, res){
+var updateDocuments = function(req, res, urlx) {
   var json = req.body;
   if(isNaN(parseInt(req.params.id)) == true){
     res.send('Wrong Input!!! (Maybe it is not number)', 400);
@@ -147,50 +91,29 @@ app.patch('/task/:id', function(req, res){
     if(typeof json.Description !== 'string'||typeof json.Completed !== 'boolean'||typeof json.Duration !== 'string'){
       res.send('Something wrong???', 400);}
     else{
-      db.collection('task').findOne(myquery,function(err, docs){
-        if(docs == null){
-          res.send('Not found :-(', 404);
-        }
-        else{
-          db.collection('task').findOneAndUpdate(myquery,newquery,{upsert: true},function(err,docs){
-            db.collection('task').findOne(myquery,function(err, docs){
+          db.collection(urlx).findOneAndUpdate(myquery,newquery,function(err,docs){
+            db.collection(urlx).findOne(myquery,function(err, docs){
               res.send(docs);
             });
           });
         }
-      });
     }
-    }
-});
+}
 
-app.patch('/work/:id', function(req, res){
-  var json = req.body;
-  if(isNaN(parseInt(req.params.id)) == true){
-    res.send('Wrong Input!!! (Maybe it is not number)', 400);
-  } else {
-  var myquery = { ID: parseInt(req.params.id)};
-  var newquery = {$set: {Description: json.Description,
-                        Completed: json.Completed,
-                        Duration: json.Duration}}
-    if(typeof json.Description !== 'string'||typeof json.Completed !== 'boolean'||typeof json.Duration !== 'string'){
-      res.send('Something wrong???', 400);}
-    else{
-      db.collection('work').findOne(myquery,function(err, docs){
-        if(docs == null){
-          res.send('Not found :-(', 404);
-        }
-        else{
-          db.collection('work').findOneAndUpdate(myquery,newquery,{upsert: true},function(err,docs){
-            db.collection('work').findOne(myquery,function(err, docs){
-              res.send(docs);
-            });
-          });
-        }
-      });
-    }
-    }
-});
-
-app.get('*', function(req, res){
-  res.send('Not found :-(', 404);
-});
+//insert
+app.post('/task', function(req, res){insertDocuments(req, res, 'task');});
+app.post('/work', function(req, res){insertDocuments(req, res, 'work');});
+//list
+app.get('/task', function(req, res){listDocuments(req, res, 'task');});
+app.get('/work', function(req, res){listDocuments(req, res, 'work');});
+//get
+app.get('/task/:id', function(req, res){getDocuments(req, res, 'task');});
+app.get('/work/:id', function(req, res){getDocuments(req, res, 'work');});
+//delete
+app.delete('/task/:id', function(req, res){deleteDocuments(req, res, 'task');});
+app.delete('/work/:id', function(req, res){deleteDocuments(req, res, 'work');});
+//update
+app.patch('/task/:id', function(req, res){updateDocuments(req, res, 'task');});
+app.patch('/work/:id', function(req, res){updateDocuments(req, res, 'work');});
+//check 404
+app.get('*', function(req, res){res.send('Not found :-(', 404);});
